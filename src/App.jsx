3de826e1b117006f8d5b4ae2c41d0487fa1ee2e5
nidhisharma20 <ns6296666@gmail.com>
2024-03-14 09:43:11 +0000

    added common layout to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,18 @@ import JobList from "./pages/JobBoard";
 import LoginPage from "./pages/LoginPage/LoginPage";
 import PrivateRoute from "./routes/PrivateRoute";
 import SignupPage from "./pages/SignupPage/SignupPage";
+import Layout from "./components/layout/Layout";
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/sign-up" element={<SignupPage />} />
-
-        <Route path="/" element={<PrivateRoute Component={JobList} />} />
-      </Routes>
+      <Layout>
+        <Routes>
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/sign-up" element={<SignupPage />} />
+          <Route path="/" element={<PrivateRoute Component={JobList} />} />
+        </Routes>
+      </Layout>
     </Router>
   );
 }
