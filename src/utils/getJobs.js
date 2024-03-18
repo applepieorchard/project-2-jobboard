78042a941ca1d20a7652bb59a2bb452955228acd
applepieorchard <paul.miclaus1229@gmail.com>
2024-03-18 15:33:14 +0000

    Created the getJobs function

--- src/utils/getJobs.js
+++ src/utils/getJobs.js
@@ -1,1 +1,10 @@
-import axios from "axios"
+import axios from "axios";
+
+export async function getJobs() {
+    const json = "./reedData.json";
+    try {
+    const res = await axios.get(json);
+    } catch (error) {
+        throw new Error(`Error fetching data from reedData.json file: ${error}`)
+    }
+};
