f0a47265db2d534d8d12a591ed02565f732a61bc
nidhisharma20 <ns6296666@gmail.com>
2024-03-20 22:51:12 +0000

    removed unwanted code

--- src/components/slider/Slide.jsx
+++ src/components/slider/Slide.jsx
@@ -9,7 +9,6 @@ import image4 from "../../assets/image4.webp";
 import image5 from "../../assets/image5.jpg";
 import FilterModal from "../UI/Modal/FilterModal";
 import { useState } from "react";
-import slider from "react-slick/lib/slider";
 // import "../../pages/jobboard.css";
 
 export default function Slide() {
